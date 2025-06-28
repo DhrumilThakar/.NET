6a2392cb04af96e0695db282e1bbd0f04465732e
Dhrumil Thakar <dhruvmilthakar@gmail.com>
2025-06-28 13:15:41 +0530

    Data type in C#

---
+++ Data Type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "The sentence we want to print";
+            char alphebate = 'a';
+            int age = 19;
+            double cgpa = 9.04;
+
+            Console.WriteLine(phrase);
+            Console.WriteLine(age);
+            Console.WriteLine(cgpa);
+            Console.WriteLine(alphebate);
+        }
+    }
+}
